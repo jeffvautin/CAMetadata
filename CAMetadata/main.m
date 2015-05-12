6d1385d131e04d66b0b926f42a7730e4281650cc
Jeff Vautin <jeff@jeffvautin.com>
2015-05-11 22:08:11 -0400

    Initial Commit

---
+++ CAMetadata/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CAMetadata
+//
+//  Created by Jeff Vautin on 5/11/15.
+//  Copyright (c) 2015 Jeff Vautin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
